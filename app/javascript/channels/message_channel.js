a85ffab7fe9c622079e5ef74bc1b40521ad98815
scheals <95084627+scheals@users.noreply.github.com>
2023-02-10 10:21:57 +0100

    feat: refactor message sending

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -18,23 +18,8 @@ const messageChannel = consumer.subscriptions.create("MessageChannel", {
                 <p>${data.user.email}</p>
               </div>
               <div class="message-body">
-                <p>${data.message.body}</p>
+                <p>${data.body}</p>
               </div>
             </article>`
   }
 });
-
-document.addEventListener("turbo:load", () => {
-  let form = document.querySelector('#message-form')
-  if(form) {
-    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      let messageInput = document.querySelector('#message-input').value
-      if(messageInput == '') return;
-      const message = {
-        body: messageInput
-      }
-      messageChannel.send({message: message})
-    })
-  }
-})
